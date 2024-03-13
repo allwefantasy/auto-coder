43f20f1fc126666a99dbc0df1994ba45280fd751
WilliamZhu <allwefantasy@gmail.com>
2024-03-13 14:10:01 +0800

    update

--- codecopilot.py
+++ codecopilot.py
@@ -10,12 +10,6 @@ from byzercopilot.pyproject import PyProject
 from byzercopilot.tsproject import TSProject
 
 
-byzerllm.connect_cluster()
-llm = byzerllm.ByzerLLM()
-llm.setup_template(model="sparkdesk_chat",template="auto")
-llm.setup_default_model_name("sparkdesk_chat")
-
-
 @byzerllm.prompt(render="jinja")
 def auto_implement_function_template(instruction:str, content:str)->str:
     '''
@@ -55,7 +49,19 @@ if __name__ == "__main__":
     parser.add_argument("--query",  help="the instruction to handle the source code")
     parser.add_argument("--template",  default="common",help="the instruction to handle the source code")
     parser.add_argument("--project_type",  default="py",help="the type of the project. py or ts")
+    parser.add_argument("--execute", action='store_false', help="Execute command line or not")
+
+    parser.add_argument("--model",  default="",help="the model name to use")
+
     args = parser.parse_args()
+
+    if args.model:
+        byzerllm.connect_cluster()
+        llm = byzerllm.ByzerLLM()
+        llm.setup_template(model=args.model,template="auto")
+        llm.setup_default_model_name(args.model)
+    else:
+        llm = None
 
     source_dir = args.source_dir
     if not os.path.exists(source_dir):
@@ -65,6 +71,7 @@ if __name__ == "__main__":
     target_file = args.target_file or None    
     template = args.template 
     project_type = args.project_type
+    should_execute = args.execute
 
     if project_type == "ts":
         pp = TSProject(source_dir=source_dir, git_url=git_url, target_file=target_file)
@@ -72,15 +79,24 @@ if __name__ == "__main__":
         pp = PyProject(source_dir=source_dir, git_url=git_url, target_file=target_file)
 
     pp.run()
-    v = pp.output()
-    with open(target_file, "w") as file:
-        content = v
-        if template == "common":
-            instruction = args.query or "Please implement the following methods"
-            content = instruction_template(instruction=instruction, content=content)
-        elif template == "auto_implement":            
-            content = auto_implement_function_template(instruction="", content=content)
-        file.write(content)
+    content = pp.output()
+    
+    if template == "common":
+        instruction = args.query or "Please implement the following methods"
+        content = instruction_template(instruction=instruction, content=content)
+    elif template == "auto_implement":            
+        content = auto_implement_function_template(instruction="", content=content)
+        
+    if should_execute:
+        t = llm.chat_oai(conversations=[{
+            "role": "user",
+            "content": content
+        }]) 
+        with open(target_file, "w") as file:        
+            file.write(t[0].output)
+        print(t[0].output)   
+
+
 
     
     
