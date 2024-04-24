53582d714f522dd4b7017703ab6b32ab889d62c7
WilliamZhu <allwefantasy@gmail.com>
2024-04-24 13:35:09 +0800

    update

--- src/autocoder/rag/simple_rag.py
+++ src/autocoder/rag/simple_rag.py
@@ -1,10 +1,14 @@
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
 from autocoder.common import SourceCode,AutoCoderArgs
 from autocoder.common.llm_rerank import LLMRerank
+import fsspec
+import os
 
 from byzerllm.apps.llama_index.simple_retrieval import SimpleRetrieval
 from byzerllm.apps.llama_index import get_service_context,get_storage_context
 from llama_index.core import QueryBundle
+from llama_index.core.readers.file.base import default_file_metadata_func,get_default_fs,_format_file_timestamp
+import mimetypes
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, ServiceContext
 from llama_index.core.node_parser import SentenceSplitter,SentenceWindowNodeParser
@@ -12,6 +16,30 @@ from llama_index.core.indices.document_summary import DocumentSummaryIndex
 from llama_index.core.base.llms.types import ChatMessage,MessageRole
 import byzerllm
 from loguru import logger
+import hashlib
+import json
+
+
+def file_metadata_func(
+    file_path: str, fs: Optional[fsspec.AbstractFileSystem] = None
+) -> Dict:
+    """Get some handy metadata from filesystem.
+
+    Args:
+        file_path: str: file path in str
+    """    
+    def generate_file_md5(file_path: str) -> str:
+        md5_hash = hashlib.md5()
+        with open(file_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(4096), b''):
+                md5_hash.update(chunk)
+        return md5_hash.hexdigest() 
+        
+    return { ** default_file_metadata_func(file_path=file_path,fs=fs),
+        "md5":generate_file_md5(file_path=file_path)
+    }
+
+
 
 class SimpleRAG:
     def __init__(self,llm:byzerllm.ByzerLLM,args:AutoCoderArgs,path:str) -> None:
@@ -29,6 +57,7 @@ class SimpleRAG:
         self.storage_context = get_storage_context(self.llm,self.retrieval,
                                                    chunk_collection=self.chunk_collection,
                                                    namespace=self.namespace)
+        self.simple_retrieval = SimpleRetrieval(llm=llm, retrieval=self.retrieval,chunk_collection=self.chunk_collection)        
 
     def stream_search(self,query:str):     
         query_bundle = QueryBundle(query_str=query)   
@@ -51,7 +80,8 @@ class SimpleRAG:
         index = VectorStoreIndex.from_vector_store(vector_store = self.storage_context.vector_store,
                                                    service_context=self.service_context)
         retrieval_engine = index.as_retriever()
-        nodes = retrieval_engine.retrieve(query_bundle)        
+        nodes = retrieval_engine.retrieve(query_bundle) 
+
         reranker = LLMRerank(llm=self.llm)
         retrieved_nodes = reranker.postprocess_nodes(nodes, query_bundle,choice_batch_size=5, top_n=1)
         return [
@@ -119,24 +149,47 @@ class SimpleRAG:
 
     def build(self):            
         retrieval_client = SimpleRetrieval(llm=self.llm,retrieval=self.retrieval)
-        retrieval_client.delete_from_doc_collection(self.namespace)
-        retrieval_client.delete_from_chunk_collection(self.chunk_collection)
+        # retrieval_client.delete_from_doc_collection(self.namespace)
+        # retrieval_client.delete_from_chunk_collection(self.chunk_collection)
         
         required_exts = self.args.required_exts or None
         documents = SimpleDirectoryReader(self.path,
                                           recursive=True,
                                           filename_as_id=True,
-                                          required_exts=required_exts).load_data()        
+                                          required_exts=required_exts,
+                                          file_metadata=file_metadata_func
+                                          ).load_data()    
+        docs_keep = []
+        for document in documents:            
+            doc = retrieval_client.get_doc(f"ref_doc_info/{document.doc_id}",self.namespace)            
+            if doc:
+                md5 = json.loads(doc["json_data"]).get("md5","")
+                file_path = document.metadata["file_path"]
+                new__md5 = document.metadata["md5"]
+                if md5 != new__md5:
+                    retrieval_client.delete_doc_and_chunks_by_filename(self.namespace,file_path)
+                    docs_keep.append(document)
+            else:
+                docs_keep.append(document)  
+
+        retrieval_client.commit_doc()
+        retrieval_client.commit_chunk()
+
+        for document in docs_keep:
+            logger.info(f'\nUpsert {document.doc_id}')
 
         sp = SentenceSplitter(chunk_size=1024, chunk_overlap=0)        
 
         nodes = sp.get_nodes_from_documents(
-            documents, show_progress=True
+            docs_keep, show_progress=True
         )
         _ = VectorStoreIndex(nodes=nodes,
                              store_nodes_override=True,
                              storage_context=self.storage_context, 
-                             service_context=self.service_context)        
+                             service_context=self.service_context) 
+
+        retrieval_client.commit_doc()
+        retrieval_client.commit_chunk()       
         
                 
         
