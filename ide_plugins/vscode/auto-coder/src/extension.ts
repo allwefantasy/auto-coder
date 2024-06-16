0700001f3827b9b5e1b19e24dcdfc4c6d903279b
WilliamZhu <allwefantasy@gmail.com>
2024-06-16 12:16:15 +0800

    update

--- ide_plugins/vscode/auto-coder/src/extension.ts
+++ ide_plugins/vscode/auto-coder/src/extension.ts
@@ -179,6 +179,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const scriptPathOnDisk = vscode.Uri.file(path.join(context.extensionPath, 'dist', 'web.js'));
   		const scriptUri = panel.webview.asWebviewUri(scriptPathOnDisk);
 		panel.webview.html = getWebviewContent(scriptUri);
+		
 		// Handle messages from the webview
 		const terminals = vscode.window.terminals;
 		let terminal;
