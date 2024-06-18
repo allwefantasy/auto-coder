f8a06e36b583194541f832c26f4f8d8da8452114
WilliamZhu <allwefantasy@gmail.com>
2024-06-18 15:17:32 +0800

    auto_coder_129_chat_action.yml_5673ff9f4d2f0b3f9c864de985c5a05f

--- src/autocoder/utils/conversation_store.py
+++ src/autocoder/utils/conversation_store.py
@@ -1,3 +1,18 @@
+def load_code_model_conversation_from_store(args: AutoCoderArgs):
+    persit_dir = os.path.join(
+        args.source_dir, ".auto-coder", "human_as_model_conversation"
+    )
+    conversation_file = os.path.join(persit_dir, "data.jsonl")
+    
+    if not os.path.exists(conversation_file):
+        return []
+    
+    conversations = []
+    with open(conversation_file, "r") as f:
+        for line in f:
+            conversations.append(json.loads(line))
+    
+    return conversations
 import os
 import json
 from autocoder.common import AutoCoderArgs
