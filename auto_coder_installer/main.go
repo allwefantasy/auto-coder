11f511d57ff2fddf394b525c455113cedf4dde31
WilliamZhu <allwefantasy@gmail.com>
2024-06-19 11:04:02 +0800

    auto_coder_162_chat_action.yml_dc505051ed5d1e30084a086f4060c084

--- auto_coder_installer/main.go
+++ auto_coder_installer/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"auto-coder/installer/cmd"
-	"auto-coder/installer/gui"
-	"runtime"
 )
 
 func main() {
-	if runtime.GOOS == "windows" {
-		gui.main()
-	} else {
-		cmd.Execute()
-	}
+	cmd.Execute()
 }
