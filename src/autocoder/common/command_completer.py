16cfb416316074abb6766c9127100ba23a82d3e8
WilliamZhu <allwefantasy@gmail.com>
2024-08-04 21:44:16 +0800

    auto_coder_pre_328_chat_action.yml_06dab2321c2f69c66c14fe1fb36969c7

---
+++ src/autocoder/common/command_completer.py
@@ -0,0 +1,7 @@
+from prompt_toolkit.completion import  Completer, Completion,CompleteEvent
+from prompt_toolkit.document import Document 
+
+class ChatCommandCompleter:    
+
+    def add_files(document:Document, complete_event:CompleteEvent):
+        pass    
