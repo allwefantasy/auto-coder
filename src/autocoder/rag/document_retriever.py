9bbbe3ba6198a6d86c145b59494c13d659475e51
WilliamZhu <allwefantasy@gmail.com>
2024-09-03 23:39:00 +0800

    update

--- src/autocoder/rag/document_retriever.py
+++ src/autocoder/rag/document_retriever.py
@@ -13,6 +13,44 @@ from autocoder.rag.loaders import (
 )
 from loguru import logger
 import threading
+from multiprocessing import Pool
+
+cache_lock = threading.Lock()
+
+def process_file(file_info: Tuple[str, str, float]) -> List[SourceCode]:
+    start_time = time.time()
+    file_path, relative_path, _ = file_info
+    try:
+        if file_path.endswith(".pdf"):
+            with open(file_path, "rb") as f:
+                content = extract_text_from_pdf(f.read())
+            v = [SourceCode(module_name=file_path, source_code=content)]
+        elif file_path.endswith(".docx"):
+            with open(file_path, "rb") as f:
+                content = extract_text_from_docx(f.read())
+            v = [SourceCode(module_name=f"##File: {file_path}", source_code=content)]
+        elif file_path.endswith(".xlsx") or file_path.endswith(".xls"):
+            sheets = extract_text_from_excel(file_path)
+            v = [
+                SourceCode(
+                    module_name=f"##File: {file_path}#{sheet[0]}",
+                    source_code=sheet[1],
+                )
+                for sheet in sheets
+            ]
+        elif file_path.endswith(".pptx"):
+            slides = extract_text_from_ppt(file_path)
+            content = "".join(f"#{slide[0]}\n{slide[1]}\n\n" for slide in slides)
+            v = [SourceCode(module_name=f"##File: {file_path}", source_code=content)]
+        else:
+            with open(file_path, "r", encoding="utf-8") as f:
+                content = f.read()
+            v = [SourceCode(module_name=f"##File: {file_path}", source_code=content)]
+        logger.info(f"Load file {file_path} in {time.time() - start_time}")
+        return v
+    except Exception as e:
+        logger.error(f"Error processing file {file_path}: {str(e)}")
+        return []
 
 
 def retrieve_documents(
@@ -56,44 +94,6 @@ def retrieve_documents(
 
         return all_files
 
-    def process_file(file_info: Tuple[str, str, float]) -> List[SourceCode]:
-        file_path, relative_path, _ = file_info
-        try:
-            if file_path.endswith(".pdf"):
-                with open(file_path, "rb") as f:
-                    content = extract_text_from_pdf(f.read())
-                return [SourceCode(module_name=file_path, source_code=content)]
-            elif file_path.endswith(".docx"):
-                with open(file_path, "rb") as f:
-                    content = extract_text_from_docx(f.read())
-                return [
-                    SourceCode(module_name=f"##File: {file_path}", source_code=content)
-                ]
-            elif file_path.endswith(".xlsx") or file_path.endswith(".xls"):
-                sheets = extract_text_from_excel(file_path)
-                return [
-                    SourceCode(
-                        module_name=f"##File: {file_path}#{sheet[0]}",
-                        source_code=sheet[1],
-                    )
-                    for sheet in sheets
-                ]
-            elif file_path.endswith(".pptx"):
-                slides = extract_text_from_ppt(file_path)
-                content = "".join(f"#{slide[0]}\n{slide[1]}\n\n" for slide in slides)
-                return [
-                    SourceCode(module_name=f"##File: {file_path}", source_code=content)
-                ]
-            else:
-                with open(file_path, "r", encoding="utf-8") as f:
-                    content = f.read()
-                return [
-                    SourceCode(module_name=f"##File: {file_path}", source_code=content)
-                ]
-        except Exception as e:
-            logger.error(f"Error processing file {file_path}: {str(e)}")
-            return []
-
     def read_cache() -> Dict[str, Dict]:
         cache = {}
         if os.path.exists(cache_file):
@@ -106,11 +106,9 @@ def retrieve_documents(
     def write_cache(cache: Dict[str, Dict]):
         with open(cache_file, "w") as f:
             for data in cache.values():
-                json.dump(data, f)
+                json.dump(data, f,ensure_ascii=False)
                 f.write("\n")
-
-    cache_lock = threading.Lock()
-
+    
     def update_cache(
         cache: Dict[str, Dict], file_info: Tuple[str, str, float], content: str
     ):
@@ -132,37 +130,36 @@ def retrieve_documents(
         if file_path not in cache or cache[file_path]["modify_time"] < modify_time:
             files_to_process.append(file_info)
 
-    num_threads = min(32, os.cpu_count() * 2)
+    with Pool(processes=os.cpu_count()) as pool:
+        results = pool.map(process_file, files_to_process)
 
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        future_to_file = {
-            executor.submit(process_file, file_info): file_info
-            for file_info in files_to_process
-        }
+    for file_info, result in zip(files_to_process, results):
+        for item in result:
+            update_cache(cache, file_info, item.source_code)
 
-        for future in as_completed(future_to_file):
-            file_info = future_to_file[future]
-            try:
-                results = future.result()
-                for result in results:
-                    with open(lock_file, "w") as lock:
-                        try:
-                            update_cache(cache, file_info, result.source_code)
-                        finally:
-                            pass
-                    yield result
-            except Exception as e:
-                logger.error(f"Error processing file {file_info[0]}: {str(e)}")
-    #MARK
+    # with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    #     future_to_file = {
+    #         executor.submit(process_file, file_info): file_info
+    #         for file_info in files_to_process
+    #     }
+
+    #     for future in as_completed(future_to_file):
+    #         file_info = future_to_file[future]
+    #         try:
+    #             results = future.result()
+    #             for result in results:
+    #                 update_cache(cache, file_info, result.source_code)
+    #         except Exception as e:
+    #             logger.error(f"Error processing file {file_info[0]}: {str(e)}")
+
     with open(lock_file, "w") as lock:
         fcntl.flock(lock, fcntl.LOCK_EX)
         try:
             write_cache(cache)
         finally:
             fcntl.flock(lock, fcntl.LOCK_UN)
-    
+
     for file_path, data in cache.items():
-        if file_path not in [f[0] for f in files_to_process]:
-            yield SourceCode(
-                module_name=f"##File: {file_path}", source_code=data["content"]
-            )
+        yield SourceCode(
+            module_name=f"##File: {file_path}", source_code=data["content"]
+        )
