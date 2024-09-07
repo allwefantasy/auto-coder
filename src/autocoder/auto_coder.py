38a679624f9b4c61c73bf0db7a2e769eae49f1f4
WilliamZhu <allwefantasy@gmail.com>
2024-09-07 21:31:49 +0800

    Revert "auto_coder_755_chat_action.yml_aec70362721d079d96aeea5a66a4d6a1"
    This reverts commit 22796f6561d5ad3c2fefc605df6d72ca27ef16a1.

--- src/autocoder/auto_coder.py
+++ src/autocoder/auto_coder.py
@@ -403,11 +403,6 @@ def main(input_args: Optional[List[str]] = None):
             chat_model.setup_default_model_name(args.chat_model)
             llm.setup_sub_client("chat_model", chat_model)            
 
-        if args.ask_model:
-            ask_model = byzerllm.ByzerLLM()
-            ask_model.setup_default_model_name(args.ask_model)
-            llm.setup_sub_client("ask_model", ask_model)
-
         if args.vl_model:
             vl_model = byzerllm.ByzerLLM()
             vl_model.setup_default_model_name(args.vl_model)
