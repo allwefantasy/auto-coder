c94af87e4d61fab634663cccbd056733cd4c5768
WilliamZhu <allwefantasy@gmail.com>
2024-09-11 20:29:17 +0800

    Revert "auto_coder_770_chat_action.yml_1554bdb7ad07b7e69fd878c270f2c749"
    This reverts commit 1d62033bbe453cb30a2429a4891636baa42a562d.

--- setup.py
+++ setup.py
@@ -28,11 +28,10 @@ setup(
     author="allwefantasy",
     long_description=readme_contents,
     long_description_content_type="text/markdown",
+    entry_points={
         'console_scripts': [
-            'auto-coder=autocoder.auto_coder:main',
-            'auto-coder.chat=autocoder.chat_auto_coder:main',
-            'auto-coder.rag=autocoder.auto_coder_rag:main',
-        ],
+            'auto-coder = autocoder.auto_coder:main',
+            'auto-coder.core = autocoder.auto_coder:main',
             'chat-auto-coder = autocoder.chat_auto_coder:main',
             'auto-coder.chat = autocoder.chat_auto_coder:main',
             'auto-coder-serve = autocoder.auto_coder_server:main',
