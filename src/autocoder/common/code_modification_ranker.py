671ba87c8a8e3583ac4c37f420b05975e3ebf49c
WilliamZhu <allwefantasy@gmail.com>
2024-12-31 17:38:58 +0800

    auto_coder_pre_000000001019_chat_action.yml_06eef073b626e1b5a1a341fb9b44303e

--- src/autocoder/common/code_modification_ranker.py
+++ src/autocoder/common/code_modification_ranker.py
@@ -6,6 +6,10 @@ from autocoder.common.code_auto_merge_editblock import CodeAutoMergeEditBlock
 from autocoder.common.code_auto_merge_strict_diff import CodeAutoMergeStrictDiff
 from autocoder.common.code_auto_merge import CodeAutoMerge
 from autocoder.common.types import CodeGenerateResult
+from pydantic import BaseModel
+
+class RankResult(BaseModel):
+    rank_result:List[int]
 
 class CodeModificationRanker:
     def __init__(self, llm: byzerllm.ByzerLLM, args: AutoCoderArgs, merger:Union[CodeAutoMergeDiff,CodeAutoMergeEditBlock,CodeAutoMergeStrictDiff,CodeAutoMerge]):
@@ -14,7 +18,7 @@ class CodeModificationRanker:
         self.merger = merger        
     
     @byzerllm.prompt()
-    def _rank_modifications(self, s:CodeGenerateResult) -> List[int]:
+    def _rank_modifications(self, s:CodeGenerateResult) -> str:
         '''
         对一组代码修改进行质量评估，并返回索引排序，最好的修改排在最前面。
 
@@ -31,13 +35,18 @@ class CodeModificationRanker:
         </edit_block>
         {% endfor %}
         
-        返回一个JSON数组，包含修改质量的排序序号，最好的修改排在最前面。
+        返回格式如下JSON数组
         
-        示例：
-        输入：["修改1", "修改2", "修改3"]
-        输出：[2, 0, 1]  # 表示修改3最好，修改1次之，修改2最差
+        ```json
+        [id3, id2, id1]
+        ```
+
+        包含修改质量的排序序号，最好的修改排在最前面
         '''
         
 
-    def rank_modifications(self, generate_result: CodeGenerateResult) -> List[int]:
-        return self._rank_modifications.with_llm(self.llm).run(generate_result)
+    def rank_modifications(self, generate_result: CodeGenerateResult) -> CodeGenerateResult:
+        v =  self._rank_modifications.with_llm(self.llm).with_return_type(RankResult).run(generate_result)
+        rerank_contents = [generate_result.contents[i] for i in v.rank_result]
+        rerank_conversations = [generate_result.conversations[i] for i in v.rank_result]
+        return CodeGenerateResult(contents=rerank_contents,conversations=rerank_conversations)
