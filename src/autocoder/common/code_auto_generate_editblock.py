4b06184cb4a8c49703fb8ac559fa71636167520e
WilliamZhu <allwefantasy@gmail.com>
2025-02-01 13:07:43 +0800

    Revert "auto_coder_000000001276_chat_action.yml_b96b8ef47b5f91093e184347742939fe"
    This reverts commit 15890cc9234d7a2f683fa34b13b1f7d6700118b0.

--- src/autocoder/common/code_auto_generate_editblock.py
+++ src/autocoder/common/code_auto_generate_editblock.py
@@ -11,8 +11,6 @@ from autocoder.utils.queue_communicate import (
 import json
 from concurrent.futures import ThreadPoolExecutor
 from autocoder.common.utils_code_auto_generate import chat_with_continue
-from autocoder.common.printer import Printer
-from autocoder.common.auto_coder_lang import get_lang
 
 
 class CodeAutoGenerateEditBlock:
@@ -30,11 +28,9 @@ class CodeAutoGenerateEditBlock:
         self.fence_0 = fence_0
         self.fence_1 = fence_1
         self.generate_times_same_model = args.generate_times_same_model
-        self.printer = Printer()
         if not self.llm:
-            self.printer.print(get_lang("Please provide a valid model instance to use for code generation."))
             raise ValueError(
-                get_lang("Please provide a valid model instance to use for code generation.")
+                "Please provide a valid model instance to use for code generation."
             )
         self.llms = self.llm.get_sub_client("code_model") or [self.llm]
         if not isinstance(self.llms, list):
