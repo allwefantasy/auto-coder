8b97de5b15c8b57d33469ee36ea3dc13283dd84c
WilliamZhu <allwefantasy@gmail.com>
2025-02-04 12:14:46 +0800

    auto_coder_pre_000000001330_chat_action.yml_8a30d3f24446ba5c7592457923bbd7d6

--- notebooks/tests/test_coder.py
+++ notebooks/tests/test_coder.py
@@ -1,22 +1,23 @@
-import os
-from openai import OpenAI
+# import os
+# from openai import OpenAI
 
-client = OpenAI(
-    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
-    api_key="", # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
-    base_url="https://qianfan.baidubce.com/v2",
-)
+# client = OpenAI(
+#     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
+#     api_key="", # 如何获取API Key：https://help.aliyun.com/zh/model-studio/developer-reference/get-api-key
+#     base_url="https://qianfan.baidubce.com/v2",
+# )
 
-completion = client.chat.completions.create(
-    model="deepseek-r1", 
-    messages=[        
-        {'role': 'user', 'content': '你是谁？'}
-        ]
-)
-print(completion.choices[0].message.content)
+# completion = client.chat.completions.create(
+#     model="deepseek-r1", 
+#     messages=[        
+#         {'role': 'user', 'content': '你是谁？'}
+#         ]
+# )
+# print(completion.choices[0].message.content)
 import time
 from typing import Generator, Tuple, Dict, Any
 from autocoder.utils.auto_coder_utils.chat_stream_out import multi_stream_out
+from byzerllm.utils.types import SingleOutputMeta
 
 
 def slow_stream() -> Generator[Tuple[str, Dict[str, Any]], None, None]:
