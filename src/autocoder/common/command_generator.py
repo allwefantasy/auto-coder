dd2a6555bc02c24a1ba3f566c44b1e45fc3c3e81
WilliamZhu <allwefantasy@gmail.com>
2025-02-06 20:51:09 +0800

    auto_coder_000000001423_chat_action.yml_257544f27f60217382dd5d2ae8bcfba2

--- src/autocoder/common/command_generator.py
+++ src/autocoder/common/command_generator.py
@@ -3,6 +3,8 @@ from byzerllm.utils.client import code_utils
 from autocoder.common import detect_env
 
 
+from autocoder.common import shells
+
 @byzerllm.prompt()
 def _generate_shell_script(user_input: str) -> str:
     """
@@ -10,6 +12,8 @@ def _generate_shell_script(user_input: str) -> str:
 
     操作系统: {{ env_info.os_name }} {{ env_info.os_version }}
     Python版本: {{ env_info.python_version }}
+    Shell类型: {{ env_info.shell_type }}
+    Shell编码: {{ env_info.shell_encoding }}
     {%- if env_info.conda_env %}
     Conda环境: {{ env_info.conda_env }}
     {%- endif %}
@@ -22,20 +26,24 @@ def _generate_shell_script(user_input: str) -> str:
     不支持Bash
     {%- endif %}
 
-    根据用户的输入以及当前的操作系统生成合适的 shell 脚本。
+    根据用户的输入以及当前的操作系统和Shell类型生成合适的 shell 脚本。
 
     用户输入: {{ user_input }}
 
-    请生成一个适当的 shell 脚本来执行用户的请求。确保脚本是安全的，并且可以在操作系统支持的 shell 中运行。
+    请生成一个适当的 shell 脚本来执行用户的请求。确保脚本是安全的,并且可以在当前Shell环境中运行。
     脚本应该包含必要的注释来解释每个步骤。
-    脚本内容请用如下方式返回：
+    脚本内容请用如下方式返回:
 
     ```shell    
     # 你的 shell 脚本内容
     ```
     """
+    env_info = detect_env()
+    # 获取当前shell类型和编码
+    env_info["shell_type"] = shells.get_terminal_name()
+    env_info["shell_encoding"] = shells.get_terminal_encoding()
     return {
-        "env_info": detect_env()
+        "env_info": env_info
     }
 
 
