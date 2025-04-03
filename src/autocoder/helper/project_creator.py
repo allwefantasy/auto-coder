63d126a3e2b9d2357ed5cdfe640ef18ef78ccdec
WilliamZhu <allwefantasy@gmail.com>
2025-04-03 13:40:42 +0700

    <revert>看看还有缺失的不？ auto_coder_000000002123_chat_action.yml e5923636ea2bba033d6a22ccd9df081a6dbf0cde

--- src/autocoder/helper/project_creator.py
+++ src/autocoder/helper/project_creator.py
@@ -107,6 +107,7 @@ class Calculator:
         main_path = os.path.join(project_dir, "main.py")
         main_content = """
 from calculator import Calculator
+  from abc import ABC, abstractmethod
 def main():
     calc = Calculator()
     
