d7ab79283605d346dadc887c3564a192ebec0b82
WilliamZhu <allwefantasy@gmail.com>
2025-04-21 19:25:39 +0800

    简化 .autocoderignore 内容赋值, 移除 @@autocoderignore_content(location: src/autocoder/auto_coder.py) 赋值时的多余括号 auto_coder_000000002536_chat_action.yml

--- src/autocoder/auto_coder.py
+++ src/autocoder/auto_coder.py
@@ -186,9 +186,7 @@ def main(input_args: Optional[List[str]] = None):
 
         # 生成 .autocoderignore 文件，采用 .gitignore 格式
         autocoderignore_path = os.path.join(source_dir, ".autocoderignore")
-        autocoderignore_content = (
-            "target\n"
-        )
+        autocoderignore_content = "target\n"
         with open(autocoderignore_path, "w", encoding="utf-8") as f:
             f.write(autocoderignore_content)
 
