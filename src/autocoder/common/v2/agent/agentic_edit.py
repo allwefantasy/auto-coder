e1cfff151e543c89590599a58b2f61a831038dc1
WilliamZhu <allwefantasy@gmail.com>
2025-05-09 22:58:24 +0800

    auto_coder_pre_000000002719_chat_action.yml

--- src/autocoder/common/v2/agent/agentic_edit.py
+++ src/autocoder/common/v2/agent/agentic_edit.py
@@ -675,17 +675,33 @@ class AgenticEdit:
         {% if extra_docs %}  
         ====
       
-        RULES PROVIDED BY USER
+        RULES OR  DOCUMENTS PROVIDED BY USER
 
         The following rules are provided by the user, and you must follow them strictly.
 
+        <user_rule_or_document_files>
         {% for key, value in extra_docs.items() %}
-        <user_rule_file>
+        <user_rule_or_document_file>
         ##File: {{ key }}
         {{ value }}
-        </user_rule_file>
-        {% endfor %}                
+        </user_rule_or_document_file>
+        {% endfor %}  
+        </user_rule_or_document_files>              
+        
         Make sure you always start your task by using the read_file tool to get the relevant RULE files listed in index.md based on the user's specific requirements.        
+        {% endif %}
+
+
+        {% if file_paths_str %}
+        ====
+        
+        FILES MENTIONED BY USER
+
+        The following are files that the user mentioned. 
+        Make sure you always start your task by using the read_file tool to get the content of the files.
+        <files>
+        {{file_paths_str}}
+        </files>
         {% endif %}
         
 
@@ -709,15 +725,7 @@ class AgenticEdit:
         3. Remember, you have extensive capabilities with access to a wide range of tools that can be used in powerful and clever ways as necessary to accomplish each goal. Before calling a tool, do some analysis within <thinking></thinking> tags. First, analyze the file structure provided in environment_details to gain context and insights for proceeding effectively. Then, think about which of the provided tools is the most relevant tool to accomplish the user's task. Next, go through each of the required parameters of the relevant tool and determine if the user has directly provided or given enough information to infer a value. When deciding if the parameter can be inferred, carefully consider all the context to see if it supports a specific value. If all of the required parameters are present or can be reasonably inferred, close the thinking tag and proceed with the tool use. BUT, if one of the values for a required parameter is missing, DO NOT invoke the tool (not even with fillers for the missing params) and instead, ask the user to provide the missing parameters using the ask_followup_question tool. DO NOT ask for more information on optional parameters if it is not provided.
         4. Once you've completed the user's task, you must use the attempt_completion tool to present the result of the task to the user. You may also provide a CLI command to showcase the result of your task; this can be particularly useful for web development tasks, where you can run e.g. \`open index.html\` to show the website you've built.
         5. The user may provide feedback, which you can use to make improvements and try again. But DO NOT continue in pointless back and forth conversations, i.e. don't end your responses with questions or offers for further assistance.                    
-        
-        {% if file_paths_str %}
-        ====
-        The following are files that the user is currently focusing on. 
-        Make sure you always start your task by using the read_file tool to get the content of the files.
-        <files>
-        {{file_paths_str}}
-        </files>
-        {% endif %}
+                
         """        
         ## auto_select_rules(context=request.user_input, llm=self.llm,args=self.args)        rules =       
         # extra_docs = get_rules()  
