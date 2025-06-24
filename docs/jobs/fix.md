e018e1713d15b04ea33464c5c15443ad8c92ccac
WilliamZhu <allwefantasy@gmail.com>
2025-06-24 17:08:19 +0800

    -mupdate

---
+++ docs/jobs/fix.md
@@ -0,0 +1,5 @@
+运行 python .auto-coder/targets/test_ac.py 运行结果打印结果必须是类似这样的：
+
+{'command': {'args': ['tdd/hello.md'], 'kwargs': {'name':'威廉'}}}
+
+如果不是，要修正 @.ac.mod.md  对应的模块的代码
--- src/autocoder/common/ac_style_command_parser/example.py
+++
@@ -1,7 +0,0 @@
-from autocoder.common.ac_style_command_parser.parser import CommandParser
-
-p = CommandParser()
-
-result = p.parse('/command "tdd/hello.md" name="威廉"')
-
-print(result)
